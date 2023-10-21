53659aef9b6e50e72d74ce29e9bcdf0e01628b8a
허남규 <heonamgyu@heonamgyuui-MacBookPro.local>
2023-10-21 11:33:56 +0900

    add route/firebase

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFbR4Hhh_zO8WCl-MUt_3aQo6OXkW3N0c",
+  authDomain: "project-b-73fe8.firebaseapp.com",
+  projectId: "project-b-73fe8",
+  storageBucket: "project-b-73fe8.appspot.com",
+  messagingSenderId: "421949192336",
+  appId: "1:421949192336:web:b4d5cf6cd02052ce57c015",
+};
+
+const app = initializeApp(firebaseConfig);
