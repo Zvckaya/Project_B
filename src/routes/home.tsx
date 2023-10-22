5dc28b4bb8a96ffade284d18868f31ab7a8af99b
허남규 <lzvckaya@gmail.com>
2023-10-22 23:49:23 +0900

    post form완료

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,12 @@
+import styled from "styled-components";
+import PostTweetForm from "../components/post-tweet-form";
+
+const Wrapper = styled.div``;
+
 export default function Home() {
   return (
-    <>
-      <div></div>
-    </>
+    <Wrapper>
+      <PostTweetForm />
+    </Wrapper>
   );
 }
